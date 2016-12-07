81e58a19314cce2264052af497cc5f93b163cbeb
Anastasiia <naryashka@rambler.ru>
2016-12-07 16:05:08 +0200

    added landing

---
+++ markup/home/Landing/js/script.js
@@ -0,0 +1,13 @@
+
+
+$("#number1").click(function(){
+	 $("#img_link1").removeClass("img_link");
+  $("#img_link1").addClass("numbers_active");
+
+});
+
+$("#number2").click(function(){
+	 $("#img_link2").removeClass("img_link");
+  $("#img_link2").addClass("numbers_active");
+
+});
