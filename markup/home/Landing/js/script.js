277d806d1694fe14b39cc9c6e9385e5f747806de
Anastasiia <naryashka@rambler.ru>
2016-12-07 16:50:00 +0200

    added landing

--- markup/home/Landing/js/script.js
+++ markup/home/Landing/js/script.js
@@ -1,13 +1,19 @@
+var date = new Date();
+
+var current = (date.getDate());
+console.log(current);
 
 
+if (current == 7) {
 $("#number1").click(function(){
 	 $("#img_link1").removeClass("img_link");
   $("#img_link1").addClass("numbers_active");
 
-});
+}); }
 
+else if (current == 13) {
 $("#number2").click(function(){
 	 $("#img_link2").removeClass("img_link");
   $("#img_link2").addClass("numbers_active");
 
-});
+}); }
