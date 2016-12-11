ff5d3fb146be022551506c0ea7121bd8395f0df3
Anastasiia <naryashka@rambler.ru>
2016-12-11 22:27:59 +0200

    added landing2

--- markup/home/Landing/js/script.js
+++ markup/home/Landing/js/script.js
@@ -1,7 +1,26 @@
 var date = new Date();
-
 var current = (date.getDate());
-console.log(current);
+var day = [14,15,16,17,18,19,20,21,22,23,24,25];
+var day1 = 14;
+var day2 = 15;
+var day3 = 16;
+var day4 = 17;
+var day5 = 18;
+var day6 = 19;
+var day7 = 20;
+var day8 = 21;
+var day9 = 22;
+var day10 = 23;
+var day11 = 24;
+var day12 = 25;
+console.log(day);
+
+for (var i = 0; i < day.length; i++) {
+	if (day[i] < current) {
+
+	}
+		
+}
 
 
 if (current == 7) {
---
+++ markup/home/Landing2/js/script.js
@@ -0,0 +1,82 @@
+var date = new Date();
+
+var current = (date.getDate());
+console.log(current);
+
+
+
+if (current == 12) {
+$("#number1").click(function(){
+	 $("#img_link1").removeClass("img_link");
+  $("#img_link1").addClass("numbers_active");
+
+
+
+}); }
+
+else if (current == 13) {
+$("#number2").click(function(){
+	 $("#img_link2").removeClass("img_link");
+  $("#img_link2").addClass("numbers_active");
+
+}); }
+
+
+if (current == 12) {
+$("#number2").click(function(){
+	 $("#img_link2").removeClass("img_link");
+  $("#img_link2").addClass("numbers_active");
+}); }
+
+
+if (current == 12) {
+$("#number3").click(function(){
+	 $("#img_link3").removeClass("img_link");
+  $("#img_link3").addClass("numbers_active");
+}); }
+
+if (current == 12) {
+$("#number4").click(function(){
+	 $("#img_link4").removeClass("img_link");
+  $("#img_link4").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number5").click(function(){
+	 $("#img_link5").removeClass("img_link");
+  $("#img_link5").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number6").click(function(){
+	 $("#img_link6").removeClass("img_link");
+  $("#img_link6").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number7").click(function(){
+	 $("#img_link7").removeClass("img_link");
+  $("#img_link7").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number8").click(function(){
+	 $("#img_link8").removeClass("img_link");
+  $("#img_link8").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number9").click(function(){
+	 $("#img_link9").removeClass("img_link");
+  $("#img_link9").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number10").click(function(){
+	 $("#img_link10").removeClass("img_link");
+  $("#img_link10").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number11").click(function(){
+	 $("#img_link11").removeClass("img_link");
+  $("#img_link11").addClass("numbers_active");
+}); }
+if (current == 12) {
+$("#number12").click(function(){
+	 $("#img_link12").removeClass("img_link");
+  $("#img_link12").addClass("numbers_active");
+}); }
